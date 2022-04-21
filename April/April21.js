8d311fcaeb5ed7d18f4cdc5ae2eae111c6d347da
crisantogo <christopher.gopeesingh@gmail.com>
2022-04-21 12:21:40 -0400

    codeWars Apr21 Solution

---
+++ April/April21.js
@@ -0,0 +1,13 @@
+// P- create a function that converts a binary number to a decimal number
+// R- return a decimal number from a string of binary numbers
+// E- '101010' should return decimal 42
+// P- create a function with a variable that accepts the arguments in the function parseInt using a radix of 2
+
+
+function binToDec(bin){
+    // ...
+    let x = parseInt(bin,2)
+    return x
+  }
+
+  console.log(binToDec('101010'))
