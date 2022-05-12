7cd82074d01631303802cd6c25dc3fe2023097ff
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-12 11:50:11 -0400

    Convert a Boolean value to a String

---
+++ May/May12.js
@@ -0,0 +1,13 @@
+// P- convert the given boolean value into its string representation.
+// R- a string 
+// E-
+// P- create a function that accepts a boolean as a parameter
+//  - return a string using the String method
+
+function booleanToString(b){
+    //your code here
+    console.log(String(b))
+  }
+
+  booleanToString(true)//'true'
+  booleanToString(false)//'false'
