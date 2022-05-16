06e5b561170ed5931c4525140e2a1d4179f06eab
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-16 08:10:51 -0400

    Convert a String to a Number

---
+++ May/May16.js
@@ -0,0 +1,13 @@
+//P- a function that can transform a string into a number.  all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+//R- A Number from a String
+//E- 
+//P- Create a function that accepts a string as paramete. Use the Number method to convert string to a number.
+// - Return the result.
+
+const stringToNumber = function(str){
+    // put your code here
+    str= Number(str)
+    console.log (str);
+  }
+
+  stringToNumber("1234") //1234
