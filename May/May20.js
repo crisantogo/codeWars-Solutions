71857030af7f434096935654164903423d036c1b
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-20 21:34:10 -0400

    Return a new Array with value(s) doubled

---
+++ May/May20.js
@@ -0,0 +1,13 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    let newarr=x.map(e=>e*2)
+    console.log(newarr)
+    
+}
+
+  maps([1, 2, 3])//[2, 4, 6]
