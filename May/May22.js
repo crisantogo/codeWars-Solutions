285aef01294811c9fef533b9490c4e06bc00a1a4
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-22 10:47:59 -0400

    Convert a Number to a String

---
+++ May/May22.js
@@ -0,0 +1,11 @@
+//P- Convert a Number to a String
+//R- A String
+//E- numberToString(67)=>'67'
+//P- 
+
+function numberToString(num) {
+    // Return a string of the number here!
+    console.log(String(num))
+  }
+
+  numberToString(67) //'67'
