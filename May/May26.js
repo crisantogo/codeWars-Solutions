06c460a3317c756a79b25ffb536909e89b79bcf6
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-26 14:08:56 -0400

    Return opposite number

---
+++ May/May26.js
@@ -0,0 +1,12 @@
+//P- Given an integer or a floating-point number, find its opposite.
+//R- Either opposite of given number
+//E-   1 : -1
+// -   14:-14
+// -  -34: 34
+//P- 
+
+function opposite(number) {
+    //your code here
+    //return number - (number * 2)s
+    return number * -1
+  }
