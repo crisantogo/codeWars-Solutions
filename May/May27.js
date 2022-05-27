e55d174a416bff1839bd972a0f7c538036b059d8
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-27 14:20:34 -0400

    Return a String of Yes or No for a boolean

---
+++ May/May27.js
@@ -0,0 +1,13 @@
+//P- Complete the method that takes a boolean value. 
+//R- return a "Yes" string for true, or a "No" string for false.
+//E- boolToWord(true), 'Yes'
+//P- 
+
+function boolToWord( bool ){
+    //...
+    return bool==true?'Yes':'No'
+  }
+
+
+boolToWord(true) //'Yes'
+boolToWord(false) //'No'
