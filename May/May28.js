2890f9085c409176f31a9ec26aeed1a5f25372b3
crisantogo <christopher.gopeesingh@gmail.com>
2022-05-28 14:47:31 -0400

    String repeat

---
+++ May/May28.js
@@ -0,0 +1,12 @@
+//P- Write a function called repeatStr which repeats the given string string exactly n times.
+//R- A String
+//E- repeatStr(6, "I") // "IIIIII"
+// - repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+//P- 
+
+function repeatStr (n, s) {
+    console.log(s.repeat(n))
+   }
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
