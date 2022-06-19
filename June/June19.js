4a6f28a55d9825de34af71d5d6bdbf0c66f1db0b
crisantogo <christopher.gopeesingh@gmail.com>
2022-06-19 12:17:47 -0400

    Return true if one of two expressions evaluate to true

---
+++ June/June19.js
@@ -0,0 +1,5 @@
+// Define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function returns true if exactly one of the two expressions evaluate to true, false otherwise.
+function xor(a, b) {
+    // TODO: Program Me
+     return a==b?false:true
+  }
