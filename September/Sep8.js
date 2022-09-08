2762611a1b8250566e0b8d56ec34733e3bbe25be
crisantogo <christopher.gopeesingh@gmail.com>
2022-09-08 17:47:24 -0400

    Add item to an Array

---
+++ September/Sep8.js
@@ -0,0 +1,8 @@
+//P- Add the value "codewars" to the websites array.
+//R- After your code executes the websites array should == ["codewars"]
+//E- ["codewars"]
+//P-
+
+let websites=[]
+websites.push('codewars')
+console.log(websites)
