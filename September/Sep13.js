dd316b97b052bb5aba5bb7cffca05b0339bdf629
crisantogo <christopher.gopeesingh@gmail.com>
2022-09-13 12:51:51 -0400

    Total amount of points

---
+++ September/Sep13.js
@@ -0,0 +1,19 @@
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// E - points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30
+// P - create a new variable whos value is an empty Array.
+//   - iterate through the given Array using .map() method.
+//   - for each iteration split the String.
+//   - from the split string compare [0] & [2] according to the parameters
+//   - push the value to empty Array.
+//   - use the .reduce() method to determine the total number of points.
+
+function points(games) {
+    // your code here
+    
+  }
+
+
+points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30
