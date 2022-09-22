983e1d3f0d7de7399a3feda28bca50148afbfc4e
crisantogo <christopher.gopeesingh@gmail.com>
2022-09-22 15:32:14 -0400

    Training JS #18

---
+++ September/Sep22.js
@@ -0,0 +1,20 @@
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+
+function splitAndMerge(string, separator) {
+    let words=string.split(' ')
+    let splitword=words.map(word=>word.split('').join(separator))
+    
+    return splitword.join(' ')
+}
+
+splitAndMerge("My name is John"," ") , "M y n a m e i s J o h n"
