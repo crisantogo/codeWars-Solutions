323b4c404ce8d9179551980777eda28537687daf
crisantogo <christopher.gopeesingh@gmail.com>
2022-10-02 11:50:48 -0400

    Goals,Goals,Goals

---
+++ October/Oct2.js
@@ -0,0 +1,10 @@
+//Complete the function to return his total number of goals in all three leagues. 
+//Note: the input will always be valid.
+//goals(43, 10, 5), 58
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+goals(43, 10, 5), 58
