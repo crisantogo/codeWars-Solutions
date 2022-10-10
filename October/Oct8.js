f3892d4033006e0e9207b069ef2029f3793e442f
crisantogo <christopher.gopeesingh@gmail.com>
2022-10-09 21:33:24 -0400

    Type of sum

---
+++ October/Oct8.js
@@ -0,0 +1,10 @@
+//Given two parameters return the type of the sum of the two arguments
+
+//Hint;
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+function typeOfSum(a, b) {
+    // good luck
+    let type= typeof(a+b)
+    return type
+}
