faf99130617217dcc7587bd6a49c896a7e4f573d
crisantogo <christopher.gopeesingh@gmail.com>
2022-10-29 19:48:39 -0400

    Classic Hello World!

---
+++ October/Oct29.js
@@ -0,0 +1,9 @@
+// Given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+class Solution{
+    static main(){
+      console.log("Hello World!")
+    }
+}
+
+Solution.main()
