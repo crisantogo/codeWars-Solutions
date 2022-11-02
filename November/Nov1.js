04c1e0f7f576145b777d0a23c88ab14a822afe00
crisantogo <christopher.gopeesingh@gmail.com>
2022-11-01 20:20:42 -0400

    Number to power

---
+++ November/Nov1.js
@@ -0,0 +1,8 @@
+// Create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times). 
+
+function numberToPower(number, power){
+    // Code here
+    return number**power
+    //Alternatively;
+    //return Math.pow(number, power)
+}
