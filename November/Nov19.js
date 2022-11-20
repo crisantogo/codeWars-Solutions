9a09a20b3b0bd6f2f6ebd00a790dcd74c824935e
crisantogo <christopher.gopeesingh@gmail.com>
2022-11-19 22:42:46 -0400

    Return to sanity

---
+++ November/Nov19.js
@@ -0,0 +1,15 @@
+// Debug;
+
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return
+//       results;
+//   }
+
+function mystery() {
+    const results = {sanity: 'Hello'};
+    console.log(results);
+}
+
+mystery()
