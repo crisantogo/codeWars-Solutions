b3d90d72cff41450df29f205aa0af0a862469dde
crisantogo <christopher.gopeesingh@gmail.com>
2022-12-24 17:11:56 -0400

    Ransom note

---
+++ Huntober/ransom.js
@@ -0,0 +1,13 @@
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
+
+const magazine =
+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+function ransomNote(str, magazine){
+
+}
+
+ransomNote("sit ad est sint", magazine), true
+ransomNote("sit ad est love", magazine), false
+ransomNote("sit ad est sint in in", magazine), true
+ransomNote("sit ad est sint in in in in", magazine), false
