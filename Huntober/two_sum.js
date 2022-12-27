10a11d3b2222279afb13fe1f6b3d41070887beed
crisantogo <christopher.gopeesingh@gmail.com>
2022-12-27 11:16:28 -0400

    Two sum

---
+++ Huntober/two_sum.js
@@ -0,0 +1,18 @@
+//Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+function twoSum(arr,sum){
+  const pairs = []
+  const nums = {}
+
+  for(const num1 of arr){
+    const num2 = sum - num1
+    if(nums[num2]){
+      pairs.push([num1,num2])
+    }else{
+      nums[num1] = 1
+    }
+  }
+  console.log(pairs)
+}
+
+twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]
