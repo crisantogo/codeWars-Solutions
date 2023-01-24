b28d5282972c77bb307b13a538ec25efe653e7aa
crisantogo <christopher.gopeesingh@gmail.com>
2023-01-23 23:15:29 -0400

    Unfinished loop

---
+++ January23/Jan23.js
@@ -0,0 +1,11 @@
+//Debug Unfinished Loop
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+}
