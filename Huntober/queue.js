b05d1775663ae17ffd14d926db869413f1da431c
crisantogo <christopher.gopeesingh@gmail.com>
2023-02-03 18:34:42 -0400

    Implementing a queue

--- Huntober/queue.js
+++ Huntober/queue.js
@@ -24,16 +24,20 @@
 
 var Queue = function() {
     // implement your Queue constructor here
+    this.list=[]
   };
   
   Queue.prototype.enqueue = function(item) {
     // add item to the queue
+    this.list.push(item)
   };
   
   Queue.prototype.dequeue = function() {
     // remove item from the front of the queue and return its value
+    return this.list.shift()
   };
   
   Queue.prototype.size = function() {
     // return number of items in queue so far
+    return this.list.length
   };
