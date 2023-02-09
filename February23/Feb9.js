115356900d818b3d956496a576a26135ff68780b
crisantogo <christopher.gopeesingh@gmail.com>
2023-02-09 08:09:32 -0400

    Century from year

---
+++ February23/Feb9.js
@@ -0,0 +1,15 @@
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    
+    return Math.ceil(year/100)
+}
+
+century(1705), 18
+century(1900), 19
