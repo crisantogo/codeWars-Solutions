6f29dd5b8a9fc39271d8a732191873af99996743
crisantogo <christopher.gopeesingh@gmail.com>
2023-02-10 08:14:29 -0400

    Jenny's secret message

---
+++ February23/Feb10.js
@@ -0,0 +1,15 @@
+//Debug
+
+// function greet(name){
+//     return "Hello, " + name + "!";
+//     if(name === "Johnny")
+//       return "Hello, my love!";
+//   }
+
+function greet(name){
+    if(name === "Johnny"){
+        return "Hello, my love!"
+    };
+    
+    return "Hello, " + name + "!";
+}
