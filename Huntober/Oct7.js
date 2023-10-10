637a5668712326a577a68aa33f6d75e433948356
crisantogo <christopher.gopeesingh@gmail.com>
2023-10-10 07:22:02 -0400

    Alphabet war

--- Huntober/Oct7.js
+++ Huntober/Oct7.js
@@ -1,10 +1,14 @@
+//7kyu
 //P- Write a function, whose parameter is 'fight', that evaluates a string to determine which string is of greater value
 //R- A string based off of which string has a greater value. If 'right side' wins return "Right side wins!". If 'left side' wins return "Left side wins!". Else return "Let's fight again!""
 //E- alphabetWar("z");        //=> Right side wins!
 //   alphabetWar("zdqmwpbs"); //=> Let's fight again!
 //   alphabetWar("zzzzs");    //=> Right side wins!
 //   alphabetWar("wwwwwwz");  //=> Left side wins!
-//P- 
+//P- Assign each variable its relevant value
+// - Create two empty arrays(left side & right side)
+// - 'split' the string parameter. Loop through the string & push the relevant values to the empty arrays
+// - 'reduce' both arrays & compare the values to determine win or draw.
 
 function alphabetWar(fight){
    return "Let's fight again!";
