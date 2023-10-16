da75af6924b97e5ddcbcc41f3bf2c7e8be00761d
crisantogo <christopher.gopeesingh@gmail.com>
2023-10-16 13:12:16 -0400

    Alphabet war

--- Huntober/Oct7.js
+++ Huntober/Oct7.js
@@ -45,8 +45,21 @@ function alphabetWar(fight){
         rightSide.push(1)
        }
     })
-    console.log(leftSide)
-    console.log(rightSide)
+   
+    leftSide=leftSide.reduce((a,b)=>a+b,0)
+    rightSide=rightSide.reduce((a,b)=>a+b,0)
+    
+    if(leftSide>rightSide){
+        console.log("Left side wins!")
+    } else if(rightSide>leftSide){
+        console.log("Right side wins!")
+    } else 
+        console.log("Let's fight again!")
+
+    // Codewars solution;   
+    // let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    // let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+    // return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";    
 }
 
 alphabetWar("z");        //=> Right side wins!
