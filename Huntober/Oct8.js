2de58f515e4d0c8d13c0ead2250fb1c08ae748ef
crisantogo <christopher.gopeesingh@gmail.com>
2023-10-17 07:10:53 -0400

    Day of the Year

---
+++ Huntober/Oct8.js
@@ -0,0 +1,16 @@
+//P- Work out what number day of the year it is given Array with the date e.g. [1,1,2023]=>1. Don't forget to account for a leap year.
+//R- A number
+//E- toDayOfYear([25, 12, 2017]), 359
+//P- 
+
+
+function toDayOfYear(arr) {
+    // return a number
+}
+
+
+toDayOfYear([25, 12, 2017]), 359
+toDayOfYear([31, 10, 1991]), 304
+toDayOfYear([1, 5, 3000]), 121
+toDayOfYear([31, 12, 2000]), 366
+toDayOfYear([31, 12, 2100]), 365
