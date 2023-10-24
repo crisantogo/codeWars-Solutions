de80a9af04e3b87f08a86fc42792171ca177b4cf
crisantogo <christopher.gopeesingh@gmail.com>
2023-10-24 07:14:34 -0400

    Day of the Year

--- Huntober/Oct8.js
+++ Huntober/Oct8.js
@@ -80,6 +80,9 @@ function toDayOfYear(arr) {
     // }
   
     // return day;
+
+    // Using Date;
+    // return (new Date(arr[2],arr[1]-1,arr[0]).getTime()-new Date(arr[2],0,0).getTime())/1000/60/60/24
 }
         
 
