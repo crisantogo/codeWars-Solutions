3cb5a0b03d4d61950805fc63b580cea2cc02a7ea
crisantogo <christopher.gopeesingh@gmail.com>
2023-10-25 07:06:12 -0400

    Spin around

---
+++ Huntober/Oct24.js
@@ -0,0 +1,8 @@
+//P- Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
+//R- A positive number
+//E- ["left", "right", "left", "right"] ➞ 0
+
+//   ["right", "right", "right", "right", "right", "right", "right", "right"] ➞ 2
+
+//   ["left", "left", "left", "left"] ➞ 1
+//P- 
