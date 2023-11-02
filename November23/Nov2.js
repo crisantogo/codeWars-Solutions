b35ad9f8a2f15b2530296c51e053e87daf6c0ff7
crisantogo <christopher.gopeesingh@gmail.com>
2023-11-02 07:33:41 -0400

    Merging sorted integer arrays

---
+++ November23/Nov2.js
@@ -0,0 +1,11 @@
+//P- Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+//R- A sorted Array
+//E- mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]
+
+function mergeArrays(a, b) {
+    // your code here
+    
+}
+
+mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]
+mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]
