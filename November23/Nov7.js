5f1b14bfe8f37ebd7c60caa8c90b84ec961f1112
crisantogo <christopher.gopeesingh@gmail.com>
2023-11-06 18:53:49 -0400

    Friend or Foe?

---
+++ November23/Nov7.js
@@ -0,0 +1,4 @@
+//P- Create a function that filters a list of strings and returns a list with only names containing 4 letters. Names must be returned in the order they appear
+//R- An Array containing string(s)
+//Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+//P
