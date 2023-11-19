f6e639d667776f4400cd6100688a74b6848778af
crisantogo <christopher.gopeesingh@gmail.com>
2023-11-19 07:29:08 -0400

    Sum of Multiples

--- November23/Nov20.js
+++ November23/Nov20.js
@@ -7,3 +7,12 @@
 //P- Create a variable whose value is 0.
 // - Loop through the numbers in the parameters, adding each value to the variable previously created
 // - Return the sum
+
+function sumMul(n,m){
+    //your idea here
+}
+
+sumMul(2, 9)      //2 + 4 + 6 + 8 = 20
+sumMul(3, 13)     //3 + 6 + 9 + 12 = 30
+sumMul(4, 123)    //4 + 8 + 12 + ... = 1860
+sumMul(4, -7)     //"INVALID"
